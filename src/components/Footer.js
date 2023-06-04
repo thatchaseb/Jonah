f20af97dbd3bc8c6f1340b8b68610cbb22151770
thatchaseb <77036934+thatchaseb@users.noreply.github.com>
2023-06-04 13:51:34 -0400

    Fixing Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div class="p-1 text-sm md:text-lg md:p-3 lg:text-xl text-[#5C3709] bg-gradient-to-l from-lightMid to-Lighter">
-      <div className='ml-[3%]'>
-        Website by Brackett Design
-      </div>
+    <div class="p-1 text-sm md:text-lg md:p-3 text-center lg:text-xl text-[#5C3709] bg-gradient-to-l from-lightMid to-Lighter">
+        Website by Brackett Website Design
     </div>
   );
 };
